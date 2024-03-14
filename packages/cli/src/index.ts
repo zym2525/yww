be955268d532bbb521094407d70c12f0dd968eb1
zhengyiming <540361168@qq.com>
2024-03-14 15:20:39 +0800

    fix: add func

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,37 @@
 export function hello() {
   return 'hello world';
 }
+
+// 冒泡算法
+export function bubbleSort(arr: number[]) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// 选择排序
+export function selectionSort(arr: number[]) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      const temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
