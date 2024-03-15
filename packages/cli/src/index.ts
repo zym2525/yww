939a324717075fdb192eb0ab3150d26a5827aa73
zhengyiming <540361168@qq.com>
2024-03-15 15:23:25 +0800

    fix: some

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -16,22 +16,3 @@ export function bubbleSort(arr: number[]) {
   }
   return arr;
 }
-
-// 选择排序
-export function selectionSort(arr: number[]) {
-  const len = arr.length;
-  for (let i = 0; i < len - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < len; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      const temp = arr[i];
-      arr[i] = arr[minIndex];
-      arr[minIndex] = temp;
-    }
-  }
-  return arr;
-}
