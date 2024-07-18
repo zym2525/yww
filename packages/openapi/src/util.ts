0bd68d9df0503d74c4aab14eead3a0152bc0d3c4
zhengyiming <540361168@qq.com>
2024-07-18 09:18:44 +0800

    fix

--- packages/openapi/src/util.ts
+++ packages/openapi/src/util.ts
@@ -7,9 +7,11 @@ import fs from 'fs';
 import * as prettier from 'prettier';
 import { camelCase, upperFirst } from 'lodash';
 import semver from 'semver';
-import { getMyNpmLatestVersion } from './pkg';
+import { getMyNpmLatestVersion as qwe } from './pkg';
 
 const { prettier: defaultPrettierOptions } = require('@umijs/fabric');
+
+const t = '1.0.0';
 
 export const getAbsolutePath = (filePath: string) => {
   if (filePath && !path.isAbsolute(filePath)) {
@@ -65,12 +67,6 @@ export const getTagName = (name: string) => {
   return name;
 };
 
-/**
- * 根据当前的数据源类型，对请求回来的 apiInfo 进行格式化
- * 如果是 op 数据源，对 tags 以及 path 中的 tags 进行处理
- * - before: 前缀（产品集.产品码） + 操作对象（必填）+ 子操作对象（可选）+ 动作（必填）
- * - after: 操作对象（必填）+ 子操作对象（可选） ==> 驼峰
- */
 export const formatApiInfo = (apiInfo: Record<string, any>): any => {
   if (
     !(
@@ -109,18 +105,7 @@ type serviceParam = {
 };
 
 type serviceParams = Record<string, serviceParam>;
-/**
- * 一方化场景下，由于 onex 会对请求的响应做处理
- *  1. 将 Response & Request 中的参数字段会变更为小驼峰写法
- *  onex 相关代码 ： http://gitlab.alipay-inc.com/one-console/sdk/blob/master/src/request.ts#L110
- *  2. 另外要注意：
- *  op 返回的数据，请求参数的类型格式 需要做额外的处理
- *  - (name) key.n, (type) string  ==> key: string []
- *  - (name) key.m,  (type) string ===>  key: string []
- *  - (name) key.key1 , (type) string ==> key: {key1:string}
- *  - (name) key.n.key1 ,(type) string => key:{ key1 :string}[]
- *  - (name) key.n.key1.m,(type) string ==> key:{key1: string[]}[]
- */
+
 export function formatParamsForYFH(
   params: serviceParams,
   paramsObject: serviceParams = {}
@@ -221,11 +206,11 @@ export class C {
   }
 
   static async init() {
-    const lastV = await getMyNpmLatestVersion();
+    const lastV = await qwe();
     this.setV(lastV);
   }
 
   static pan() {
-    return semver.lt(this.v, '1.0.0');
+    return semver.lt(this.v, t);
   }
 }
